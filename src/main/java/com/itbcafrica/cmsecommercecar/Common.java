84460c66190436f3324b49eba49efcec7f894e00
bouendeu <raphael.bouendeu@itbcafrica.com>
2019-10-13 23:06:52 +0200

    second commit u

--- src/main/java/com/itbcafrica/cmsecommercecar/Common.java
+++ src/main/java/com/itbcafrica/cmsecommercecar/Common.java
@@ -25,9 +25,6 @@ public class Common {
 
 	@Autowired
 	private CategoryRepository categoryRepo;
-	@Autowired
-	PageRepository pageRepository;
-
 
 	@ModelAttribute
 	public void sharedData(Model model, HttpSession session, Principal principal) {
@@ -35,8 +32,6 @@ public class Common {
 		if (principal != null) {
 			model.addAttribute("principal", principal.getName());
 		}
-		Page page = pageRepository.findBySlug("home");
-		model.addAttribute("page", page);
 
 		List<Page> pages = pageRepo.findAllByOrderBySortingAsc();
 		List<Category> categories = (List<Category>) categoryRepo.findAll();
